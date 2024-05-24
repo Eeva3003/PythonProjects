90b7fc6c1da2ef2f58298609438006c9bdea4a2e
Eeva3003 <97470217+Eeva3003@users.noreply.github.com>
2024-05-24 23:04:21 +0530

    Create main1.py

---
+++ 100Movies_Webscraping/main1.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+URL="https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+response=requests.get(URL)
+website_html=response.text
+
+soup=BeautifulSoup (website_html ,"html.parser")
+all_m=soup.find_all(name="h3",class_="title")
+
+movie_title=[movie.getText() for movie in all_m]
+movies=movie_title[::-1]
+with open("movies.txt",mode="w") as file:
+    for movie in movies:
+        file.write(f'{movie} \n')
